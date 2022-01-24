1b3e13eea791bbffdb0f3d37fc7c21678a5fa7d3
Asus <saturniesm@gmail.com>
2022-01-25 00:33:43 +0700

    updating several views

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -5,12 +5,52 @@ class Home extends Controller
 
  public function index()
  {
-  //  nulis alamat yang akan ditulis di sini
+  $data['judul'] = 'Rinse';
+
+  $this->view('partials/header', $data);
+  $this->view('partials/navbar');
+  $this->view('partials/sidebar');
   $this->view('home/index');
+  $this->view('partials/footer');
+
  }
 
- public function ikan()
+ public function keuangan()
  {
+
+  $data['judul'] = 'Keuangan';
+
+  $this->view('partials/header', $data);
+  $this->view('partials/navbar');
+  $this->view('partials/sidebar');
+  $this->view('home/keuangan');
+  $this->view('partials/footer');
+
+ }
+
+ public function transaksi()
+ {
+
+  $data['judul'] = 'Transaksi';
+
+  $this->view('partials/header', $data);
+  $this->view('partials/navbar');
+  $this->view('partials/sidebar');
+  $this->view('home/transaksi');
+  $this->view('partials/footer');
+
+ }
+
+ public function ringkasan()
+ {
+
+  $data['judul'] = 'Ringkasan';
+
+  $this->view('partials/header', $data);
+  $this->view('partials/navbar');
+  $this->view('partials/sidebar');
+  $this->view('home/ringkasan');
+  $this->view('partials/footer');
 
  }
 }
