29d172acafb19ecfecde7fa2f55dba56d757c5da
Asus <saturniesm@gmail.com>
2022-01-27 09:16:15 +0700

    checking gitignore

--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -15,28 +15,65 @@ class Database
 
   $dsn = 'mysql:host=' . $this->__dbHost . ';dbname=' . $this->__dbName;
 
-  $option = [
-    PDO::ATTR_PERSISTENT => true,
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-  ];
+  $option = array(
+   PDO::ATTR_PERSISTENT => true,
+   PDO::ATTR_ERRMODE    => PDO::ERRMODE_EXCEPTION,
+  );
 
-  try
-  {
+  try {
    $this->__dbHandler = new PDO($dsn, $this->__dbUser, $this->__dbPass, $option);
-  } catch (PDOException $__error) {
-   die($__error->getMessage());
+  } catch (PDOException $error) {
+   $this->__error = $error->getMessage();
+   echo $this->__error;
   }
  }
 
  public function query($query)
  {
-   $this->__statement = $this->__dbHandler->prepare($query);
+  $this->__statement = $this->__dbHandler->prepare($query);
  }
 
- public function($param, $value, $type = null)
+ public function bind($param, $value, $type = null)
  {
-   
+  if (is_null($type)) {
+   switch (true) {
+    case is_int($value):
+     $type = PDO::PARAM_INT;
+     break;
+    case is_bool($value):
+     $type = PDO::PARAM_BOOL;
+     break;
+    case is_null($value):
+     $type = PDO::PARAM_NULL;
+     break;
+    default:
+     $type = PDO::PARAM_STR;
+   }
+  }
+  $this->stmt->bindValue($param, $value, $type);
  }
 
+ public function execute()
+ {
+  $this->__statement->execute();
+  return $this->__statement->fetchAll(PDO::FETCH_OBJ);
+ }
+
+ public function resultSet()
+ {
+  $this->execute();
+  return $this->__statement->fetchAll(PDO::FETCH_ASSOC);
+ }
+
+ public function single()
+ {
+  $this->execute();
+  return $this->__statement->fetch(PDO::FETCH_ASSOC);
+ }
+
+ public function rowCount()
+ {
+  return $this->__statement->rowCount();
+ }
 
 }
